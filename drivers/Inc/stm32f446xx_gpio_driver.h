e7c5c36d10597f9b0d31833f89dc1ee44523932e
mynamasp <prasannavenkatesh918@gmail.com>
2020-08-05 21:45:51 +0530

    User Macros and Gpio Clock En/Disable Function Added

--- drivers/Inc/stm32f446xx_gpio_driver.h
+++ drivers/Inc/stm32f446xx_gpio_driver.h
@@ -34,6 +34,68 @@ typedef struct
 }GPIO_Handle_t;
 
 
+/*
+ * GPIO Possible Pin Numbers
+ */
+
+#define GPIO_PIN_NO_0		0
+#define GPIO_PIN_NO_1		1
+#define GPIO_PIN_NO_2		2
+#define GPIO_PIN_NO_3		3
+#define GPIO_PIN_NO_4		4
+#define GPIO_PIN_NO_5		5
+#define GPIO_PIN_NO_6		6
+#define GPIO_PIN_NO_7		7
+#define GPIO_PIN_NO_8		8
+#define GPIO_PIN_NO_9		9
+#define GPIO_PIN_NO_1O		10
+#define GPIO_PIN_NO_11		11
+#define GPIO_PIN_NO_12		12
+#define GPIO_PIN_NO_13		13
+#define GPIO_PIN_NO_14		14
+#define GPIO_PIN_NO_15		15
+
+
+
+/*
+ * GPIO Possible Pin Modes
+ */
+
+#define GPIO_MODE_IN		0
+#define GPIO_MODE_OUT		1
+#define GPIO_MODE_ALTFN		2
+#define GPIO_MODE_ANALOG	3
+#define GPIO_MODE_IT_FT		4
+#define GPIO_MODE_IT_RT		5
+#define GPIO_MODE_IT_RFT	6
+
+
+/*
+ * GPIO Possible Pin Output Type
+ */
+
+#define GPIO_OP_TYPE_PP		0
+#define GPIO_OP_TYPE_OD		1
+
+
+/*
+ * GPIO Possible Pin Output Speed
+ */
+
+#define GPIO_SPEED_LOW		0
+#define GPIO_SPEED_MEDIUM	1
+#define GPIO_SPEED_FAST		2
+#define GPIO_SPEED_HIGH		3
+
+
+/*
+ * GPIO Pin Pull-up And Pull Down Configuration	Macros
+ */
+
+#define GPIO_NO_PUPD		0
+#define GPIO_PIN_PU			1
+#define GPIO_PIN_PD			2
+
 /******************************************************************************************
  *								APIs supported by this driver
  *		 For more information about the APIs check the function definitions
--- drivers/Src/stm32f44xx_gpio_driver.c
+++ drivers/Src/stm32f44xx_gpio_driver.c
@@ -23,6 +23,58 @@
  */
 void GPIO_PeriClockControl(GPIO_RegDef_t *pGPIOx,uint8_t EnorDi){
 
+	if(EnorDi == ENABLE){
+		if(pGPIOx == GPIOA){
+			GPIOA_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOB){
+			GPIOB_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOC){
+			GPIOC_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOD){
+			GPIOD_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOE){
+			GPIOE_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOF){
+			GPIOF_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOG){
+			GPIOG_PCLK_EN();
+		}
+		else if (pGPIOx == GPIOH){
+			GPIOH_PCLK_EN();
+		}
+	}
+	else{
+		if(pGPIOx == GPIOA){
+					GPIOA_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOB){
+					GPIOB_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOC){
+					GPIOC_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOD){
+					GPIOD_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOE){
+					GPIOE_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOF){
+					GPIOF_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOG){
+					GPIOG_PCLK_DI();
+				}
+				else if (pGPIOx == GPIOH){
+					GPIOH_PCLK_DI();
+				}
+	}
 }
 
 /*********************************************************************
